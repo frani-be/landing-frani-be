f8160756ead34484911c3ce4f2e482cc2fd269d1
Francisca Beatriz Medina Concha <fbmedinaconcha@gmail.com>
2024-06-19 23:45:58 -0400

    main-image base

---
+++ assets/scripts/image-transition.js
@@ -0,0 +1,28 @@
+let counter = 1;
+const cantImages = 5;
+const duration = 13000; // La duración total de la animación es 13 segundos
+const initialDelay = 2000; // El span 1 tiene un delay de -8s, lo que significa que entra a los 5s
+const fadeDuration = 500; // Duración de la transición de opacidad
+
+const mainImage = document.getElementById('main-image');
+
+// Función para cambiar la imagen
+function changeImage() {
+  counter = counter % cantImages + 1;
+  mainImage.style.opacity = 0; // Comienza el efecto de desvanecimiento
+
+  // Cambia la imagen después de que la opacidad llegue a 0
+  setTimeout(() => {
+    mainImage.src = `assets/images/image_fran-${counter}.png`;
+    mainImage.style.opacity = 1; // Vuelve a establecer la opacidad a 1 para mostrar la nueva imagen
+  }, fadeDuration); 
+  
+}
+
+// Inicializa la primera imagen visible y luego cambia cada 13 segundos
+mainImage.style.opacity = 1;
+// Ajusta el primer cambio para que coincida con la animación del primer span
+setTimeout(() => {
+  changeImage();
+  setInterval(changeImage, duration);
+}, duration - initialDelay); // Comienza el primer cambio de imagen cuando el primer span hace slideRight
