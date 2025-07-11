9f10b3bfcecd6a4b4d357681c208942e8a137f52
Francisca Beatriz Medina Concha <fbmedinaconcha@gmail.com>
2025-07-11 02:39:42 -0400

    feat: add footer with social links and current year display

---
+++ assets/scripts/curren-year.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const currentYearElement = document.getElementById('current-year');
+    if (currentYearElement) {
+        const currentYear = new Date().getFullYear();
+        currentYearElement.textContent = currentYear;
+    }
+});
